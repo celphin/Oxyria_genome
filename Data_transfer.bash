58fb92d7c8e9ad513b58f7b93794d895a64fd266
celphin <cassandra.elphinstone@shaw.ca>
2025-08-10 22:21:09 -0700

    Create Data_transfer.bash

---
+++ Data_transfer.bash
@@ -0,0 +1,16 @@
+﻿#######################
+# Transferring salmonberry data to shared folder
+# July 2025
+########################
+
+# directory needs to be under scratch
+# on Beluga
+cd /scratch/vschimma/rosaceae_phylogeny/GeneSpace/Rosaceae_genomes
+
+cd /home/celphin/scratch/Oxyria/GeneSpace/Rosaceae_genomes
+
+
+cp -vr /home/celphin/scratch/Oxyria/GeneSpace/Rosaceae_genomes/bed /scratch/vschimma/rosaceae_phylogeny/GeneSpace/Rosaceae_genomes
+cp -vr /home/celphin/scratch/Oxyria/GeneSpace/Rosaceae_genomes/peptide /scratch/vschimma/rosaceae_phylogeny/GeneSpace/Rosaceae_genomes
+cp -vr /home/celphin/scratch/Oxyria/GeneSpace/Rosaceae_genomes/genomes /scratch/vschimma/rosaceae_phylogeny/GeneSpace/Rosaceae_genomes
+
